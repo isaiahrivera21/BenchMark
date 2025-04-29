11138044a287086a595105bbfd7dc31d65917e00
isaiahrivera21 <isaiahcooperdump@gmail.com>
2025-04-29 12:55:30 -0400

    Now we can preopulate a database with some exercises

--- exerciserepo/management/commands/populateexercise.py
+++ exerciserepo/management/commands/populateexercise.py
@@ -1,11 +1,12 @@
 # should populate the food database to have intial values. 
 
 from django.core.management.base import BaseCommand, CommandError
-from exerciserepo.models import Exercise
+from exerciserepo.models import ExerciseEntry
 from dotenv import load_dotenv
 import os
 import requests
 from pprint import pprint
+from django.db import transaction
 
 
 
@@ -16,14 +17,9 @@ class Command(BaseCommand):
 
 
     def handle(self,*args,**kwargs):
+        PAGE = 0
         self.stdout.write("Starting to pull data from exercise api")
         all_exercises = []
-        PAGE = 0
-        
-        # load_dotenv()
-        # api_key = os.getenv("USDA_API_KEY")
-        # # demo_key = 'DEMO_KEY'
-
         api_url = "https://exercisedb-api.vercel.app/api/v1/exercises"
         offset = 0
         limit = 100  # Adjust as needed
@@ -41,7 +37,7 @@ class Command(BaseCommand):
                     break
 
                 data = response.json()['data']
-                exercises = data['exercises']
+                exercises = data.get('exercises', [])
                 all_exercises.extend(exercises)
 
                 if not data['nextPage']:  # Stop if there's no next page
@@ -51,10 +47,24 @@ class Command(BaseCommand):
 
                 PAGE += 1
         
-            breakpoint() # <-- sp this get us what we want. Now we have to find a way to bulk insert it into the database
+            exercise_instances = [
+                ExerciseEntry(
+                    exercise_name=item.get('name', ''),  
+                    target_muscles=item.get('targetMuscles', '') 
+                )
+                for item in all_exercises
+            ]
 
+            ExerciseEntry.objects.bulk_create(exercise_instances)
 
-        
+            self.stdout.write(self.style.SUCCESS("Successfully populated exercise database"))
+
         except requests.exceptions.RequestException as e:
             # should send stuff to std err 
             self.stderr.write(e)
+
+# for reference if we easily want to pull data we can do 
+# all_exercises[0]['name'] + all_exercises[0]['targetMuscles']
+
+# but maybe there is a better way to froup this together and bulk insert it 
+# all_exercises.extend(ExerciseEntry(exercise_name = "p", target_muscles = "q"))
